df9956a98d4864aa0ae69e01b714e0163eb8521d
Mike <mblack101@ufl.edu>
2018-11-07 12:41:01 -0500

    updating comments

--- src/readability/checker/RCTracker.java
+++ src/readability/checker/RCTracker.java
@@ -13,11 +13,8 @@ public class RCTracker {
 	private int numWords;
   private int numSentences;
   private int numSyllables;
-  private int numClosingPTags;
   private int numIPs;
-  private int numLists;
-  private int numNBSPs;
-	
+
   // calculated from the counter values
   private double numWordsPerSentence;
   private double numSyllablesPerWord;
@@ -26,42 +23,22 @@ public class RCTracker {
   private double FKGrade;
   private double FKScore;
 
-  // constructor initializes counters
+  // constructor reinitializes counters
   public RCTracker() {
     numWords = 0;
     numSentences = 0;
     numSyllables = 0;
-    numClosingPTags = 0;
     numIPs = 0;
-    numLists = 0;
-    numNBSPs = 0;
     numWordsPerSentence = 0;
     numSyllablesPerWord = 0;
     FKGrade = 0;
     FKScore = 0;
   } 
-  
-// getters
-public Integer getNumWords() {return this.numWords;}
-public Integer getNumSentences() {return this.numSentences;}
-public Integer getNumSyllables() {return this.numSyllables;}
-public Integer getNumIPs() {return this.numIPs;}
-public Double getNumWordsPerSentence() {return numWordsPerSentence;} 
-public Double getNumSyllablesPerWord() {return numSyllablesPerWord;} 
-public Double getFKScore() {return this.FKScore;}
-public Double getFKGrade() {return this.FKGrade;}
 
-// setters
-public void setNumWords(int numWords) {this.numWords = numWords;}
-public void setNumSentences(int numSentences) {this.numSentences = numSentences;}
-public void setNumSyllables(int numSyllables) {this.numSyllables = numSyllables;}
-public void setNumIPs(int numIPs) {this.numIPs = numIPs;}
-public void setNumWordsPerSentence(double numWordsPerSentence) {this.numWordsPerSentence = numWordsPerSentence;} 
-public void setNumSyllablesPerWord(double numSyllablesPerWord) {this.numSyllablesPerWord = numSyllablesPerWord;} 
-public void setFKScore(double FKScore) {this.FKScore = FKScore;}
-public void setFKGrade(double FKGrade) {this.FKGrade = FKGrade;}
-
-
+/**
+ * Adjusts the number of words and sentences to avoid double
+ * counting of certain items
+ */
 public void applyModifiers() {
     if (numIPs > 0) {
       // Don't count every octet of an IP address as a word
@@ -77,18 +54,27 @@ public void applyModifiers() {
     }
 }
 	
+	/**
+	 * Calculates the number of words per sentence
+	 */
   public void calculateNumWordsPerSentence() {
     if (numSentences > 0) {		
       setNumWordsPerSentence((double)getNumWords()/(double)getNumSentences());
     }
 	}
 	
+	/**
+	 * Calculates the number of syllable per word
+	 */
 	public void calculateNumSyllablesPerWord() {
     if (numWords > 0) {
       setNumSyllablesPerWord((double)numSyllables/(double)numWords);
     }
   } 
   
+	/**
+	 * Calculates the Flesch-Kincaid Readability Score
+	 */
   public void calculateFKScore() {
     // calculate the Flesch-Kincaid Score
     FKScore = (206.835 - (1.015 * numWordsPerSentence) - (84.6 * numSyllablesPerWord));
@@ -100,11 +86,48 @@ public void applyModifiers() {
 		} 
   }
 
+	/**
+	 * Calculates the Flesch-Kincaid Readability Grade
+	 */
   public void calculateFKGrade() {
     // calculate the Flesch-Kincaid Grade
     FKGrade = ((0.39 * numWordsPerSentence) + (11.8 * numSyllablesPerWord - 15.59));
     if (FKGrade < 0) {
 			FKGrade = 0;
 		} //minimum score = 0
-  }	
+  }
+
+// getters and setters
+public Integer getNumWords() {return this.numWords;}
+
+public void setNumWords(int numWords) {this.numWords = numWords;}
+
+public Integer getNumSentences() {return this.numSentences;}
+
+public void setNumSentences(int numSentences) {this.numSentences = numSentences;}
+
+public Integer getNumSyllables() {return this.numSyllables;}
+
+public void setNumSyllables(int numSyllables) {this.numSyllables = numSyllables;}
+
+public Integer getNumIPs() {return this.numIPs;}
+
+public void setNumIPs(int numIPs) {this.numIPs = numIPs;}
+
+public Double getNumWordsPerSentence() {return numWordsPerSentence;} 
+
+public void setNumWordsPerSentence(double numWordsPerSentence) {this.numWordsPerSentence = numWordsPerSentence;} 
+
+public Double getNumSyllablesPerWord() {return numSyllablesPerWord;} 
+
+public void setNumSyllablesPerWord(double numSyllablesPerWord) {this.numSyllablesPerWord = numSyllablesPerWord;} 
+
+public Double getFKScore() {return this.FKScore;}
+
+public void setFKScore(double FKScore) {this.FKScore = FKScore;}
+
+public Double getFKGrade() {return this.FKGrade;}
+
+public void setFKGrade(double FKGrade) {this.FKGrade = FKGrade;}
+	
 }
